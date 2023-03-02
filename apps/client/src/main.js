fb94169a0a9df98c9d965a951267bffbf779c240
Paul Su <paul.su@ohbottech.com>
2023-03-02 18:53:47 +0800

    chore: add app vite config

--- apps/client/src/main.js
+++ apps/client/src/main.js
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue-demi'
+
+import 'uno.css'
 import './style.css'
 import App from './App.vue'
 
--- apps/client/vite.config.js
+++ apps/client/vite.config.js
@@ -1,7 +1,54 @@
+/* eslint-disable node/no-path-concat */
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'url'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import UnoCSS from 'unocss/vite'
+import transformerDirectives from '@unocss/transformer-directives'
+import postcssNested from 'postcss-nested'
+import fs from 'fs'
+// const fs = require('fs')
+
+const https = () => {
+  const useHttps = (process.env.npm_lifecycle_event || '').includes(':https')
+  const keyPem = `${__dirname}/localhost-key.pem`
+  const certPem = `${__dirname}/localhost.pem`
+  if (!useHttps) return {}
+  if (!fs.existsSync(keyPem) || !fs.existsSync(certPem)) return {}
+  return {
+    https: {
+      key: fs.readFileSync(keyPem),
+      cert: fs.readFileSync(certPem),
+    },
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    UnoCSS({
+      transformers: [
+        transformerDirectives(), // https://github.com/unocss/unocss/issues/1570
+      ],
+    }),
+  ],
+  css: {
+    postcss: {
+      plugins: [
+        postcssNested,
+      ],
+    },
+  },
+  server: {
+    ...https(),
+    cors: true,
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
